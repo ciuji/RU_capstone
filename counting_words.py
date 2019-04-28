6b469e60257d9fd6e1776f46842f35bd480d0db2
Mo Shi <mo.shi@rutgers.edu>
2019-04-28 17:18:16 -0400

    Add files via upload
    Put the script under the folder that contains all .txt files. Run it, and the number of words will be shown.

---
+++ counting_words.py
@@ -0,0 +1,19 @@
+import os
+
+def count(s):
+	return len(s.split())
+
+if __name__ == '__main__':
+	index = 0
+	path = os.path.split(os.path.realpath(__file__))[0]
+	words = 0
+	while True:
+		index += 1
+		try:
+			f = open(path + '\\' + f'{index}.lab', 'r')
+			s = f.read()
+			f.close()
+			words += count(s)
+		except FileNotFoundError:
+			break
+	print(f'The number of words in total is: {words}')
